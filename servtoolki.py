aaddd1f4df8dbe07c1b3533f1d9670caa9108c52
Unsoftik <gg.unsoftik@gmail.com>
2025-05-10 19:19:35 +0300

    Update servtoolki.py

--- servtoolki.py
+++ servtoolki.py
@@ -131,5 +131,5 @@ def login():
         return jsonify({"error": "Неверный пароль"}), 401
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway передаёт свой порт через переменную окружения
+    port = int(os.environ.get("PORT", 8080))  # Railway передаёт свой порт через переменную окружения
     app.run(host="0.0.0.0", port=port)
