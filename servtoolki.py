d607cb8577303644eefbdc5f1a6e2aaf10301019
Unsoftik <gg.unsoftik@gmail.com>
2025-05-10 18:54:34 +0300

    Update servtoolki.py

--- servtoolki.py
+++ servtoolki.py
@@ -125,4 +125,6 @@ def login():
 
 if __name__ == "__main__":
     init_data()
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # <-- важно для Railway
+    app.run(host="0.0.0.0", port=port)
+
